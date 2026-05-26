83fb5a305452b362298355e51c80859cbfc76f93
GabrielSolinos <gabrielportosolinos2004@gmail.com>
2026-05-26 16:45:44 +0100

    Portal E Menus

--- Assets/Pac-Mon/script/PacMon.cs
+++ Assets/Pac-Mon/script/PacMon.cs
@@ -6,23 +6,24 @@ using UnityEngine.AI;
 public class PacMon : MonoBehaviour
 {
     private NavMeshAgent agent;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
         agent.SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
     }
 
-     void OnCollisionEnter(Collision col)
+    void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.tag == "Player")
+        if (col.gameObject.CompareTag("Player"))
         {
-            transform.position = new Vector3(0, 0, 0);
+             
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOverScreen");
+            //transform.position = new Vector3(0, 0, 0);
         }
     }
 }
